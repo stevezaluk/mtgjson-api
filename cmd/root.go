7447f2fa07eefb1b4feac5800000f5246e51c741
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-13 22:05:31 -0700

    Updated short and long descriptions for the root command

--- cmd/root.go
+++ cmd/root.go
@@ -11,13 +11,8 @@ var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "mtgjson",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "An RESTful API built on top of the MTGJSON data set",
+	Long:  `This tool allows you to control the execution of the MTGJSON API`,
 }
 
 func Execute() {
