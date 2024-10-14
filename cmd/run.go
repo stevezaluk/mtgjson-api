f64758cb809af4c038c310a110d30056427e0681
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-14 15:43:40 -0700

    Implemented functionality for the run command. The context is initialized in the PreRun block of the command and execution is started during the Run block. The PostRun block will call the destructor to disconnect from the Database

--- cmd/run.go
+++ cmd/run.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
-	"fmt"
-
+	// "context"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
+	"mtgjson/api"
+	"mtgjson/server"
 )
 
 var defaultConfig string = "~/.config/mtgjson/config.json"
+var ServerConfig server.Config
 
 var runCmd = &cobra.Command{
 	Use:   "run",
@@ -19,14 +22,23 @@ $ mtgjson run -c /path/to/config/.json
 
 To start the API using environmental variables
 $ mtgjson run --env`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		api.InitConfig(cmd.PersistentFlags())
+		api.InitDatabase()
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("[error] run not implemented yet")
+		router := gin.Default()
+		router.GET("/health", api.HealthGET)
+		router.Run()
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-	runCmd.Flags().StringP("config", "c", defaultConfig, "The path to your MTGJSON config file")
-	runCmd.Flags().BoolP("env", "e", false, "Ignore the default config path and attempt to use Environmental Variables")
-	runCmd.Flags().Int64P("port", "p", 2100, "Set the default port that the API listens on")
+	runCmd.PersistentFlags().StringP("config", "c", defaultConfig, "The path to your MTGJSON config file")
+	runCmd.PersistentFlags().BoolP("env", "e", false, "Ignore the default config path and attempt to use Environmental Variables")
+	runCmd.PersistentFlags().Int64P("port", "p", 2100, "Set the default port that the API listens on")
 }
