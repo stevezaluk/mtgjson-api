f181792352a214174163eb46af8b4bb6cad06c05
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-14 11:42:46 -0700

    Moved middleware code to a seperate package to avoid circular imports

--- api/middleware.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "github.com/gin-gonic/gin"
-
-func MiddlewareDatabase(c *gin.Context) {
-
-}
---
+++ middleware/middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func MiddlewareConfig(c *gin.Context) {
+
+}
+
+func MiddlewareDatabase(c *gin.Context) {
+
+}
