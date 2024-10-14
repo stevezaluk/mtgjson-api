0c1b91c4497b99e1488ed10950b87d4dbb9625ef
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-14 15:42:24 -0700

    Implemented a basic background context here. This stores the config and database structs so that we can re-use the same connection across each request that is made. Proper destructor still needs to be implemented

---
+++ api/context.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/pflag"
+	"mtgjson/server"
+)
+
+var ServerContext = context.Background()
+
+func InitConfig(flags *pflag.FlagSet) {
+	var config server.Config
+	env, err := flags.GetBool("env")
+	if err != nil {
+		fmt.Println("[error] Error with env flag: ", err)
+	}
+
+	if env {
+		config.ParseFromEnv()
+	} else {
+		configPath, err := flags.GetString("config")
+		if err != nil {
+			fmt.Println("[error] Error with config flag: ", err)
+		}
+
+		config.Parse(configPath)
+	}
+
+	ctx := context.WithValue(ServerContext, "config", config)
+	ServerContext = ctx
+}
+
+func InitDatabase() {
+	var database server.Database
+	database.Config = ServerContext.Value("config").(server.Config)
+
+	database.Connect() // externalize errors to here instead of printing
+
+	ctx := context.WithValue(ServerContext, "database", database)
+	ServerContext = ctx
+}
