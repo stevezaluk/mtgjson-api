97e4da7176716c930af5908e93f92f34f5995a69
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-13 22:21:22 -0700

    Implemented the env flag. This allows the user to parse a config through environmental variables instead of the config

--- cmd/run.go
+++ cmd/run.go
@@ -12,13 +12,13 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run the API in the foreground and use STDOUT for logging",
 	Long: `To start the API using the default config path:
-mtgjson run
+$ mtgjson run
 
 To start the API using a custom config file:
-mtgjson run -c /path/to/config/.json
+$ mtgjson run -c /path/to/config/.json
 
 To start the API using environmental variables
-mtgjson run --env`,
+$ mtgjson run --env`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("[error] run not implemented yet")
 	},
@@ -27,4 +27,6 @@ mtgjson run --env`,
 func init() {
 	rootCmd.AddCommand(runCmd)
 	runCmd.Flags().StringP("config", "c", defaultConfig, "The path to your MTGJSON config file")
+	runCmd.Flags().BoolP("env", "e", false, "Ignore the default config path and attempt to use Environmental Variables")
+
 }
