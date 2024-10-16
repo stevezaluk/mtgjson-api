3980d66d5276cfbdde504ec662450d897e080fa8
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-16 11:05:46 -0400

    Added additional errors for failing to find a deck

--- errors/errors.go
+++ errors/errors.go
@@ -2,6 +2,14 @@ package errors
 
 import "errors"
 
+/*
+Card Errors - Holds all errors that could arise from fetching or inserting cards
+*/
 var ErrNoCard = errors.New("card: failed to find card with specified uuid")
 var ErrNoCards = errors.New("card: No card found on index operation")
 var ErrInvalidUUID = errors.New("card: invalid v5 uuid")
+
+/*
+Deck Errors - Holds all errors that could arise from fetching or inserting decks
+*/
+var ErrNoDeck = errors.New("deck: failed to find deck with the specified code")
