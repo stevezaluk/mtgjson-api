d4cc508827abe38502cdd76e291e1756d6c5d729
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-16 23:24:00 -0400

    Implemented full support for making delete requests to the /deck/contents endpoint

--- api/deck.go
+++ api/deck.go
@@ -93,3 +93,41 @@ func DeckContentPUT(c *gin.Context) {
 
 	c.JSON(http.StatusAccepted, gin.H{"message": "Successfully updated deck", "deckCode": code, "count": len(updates.UUID)})
 }
+
+func DeckContentDELETE(c *gin.Context) {
+	code := c.Query("deckCode")
+	if code == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Deck code is required to fetch a deck's contents"})
+		return
+	}
+
+	_deck, err := deck.GetDeck(code)
+	if err == errors.ErrNoDeck {
+		c.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+
+	type DeckUpdate struct {
+		UUID []string
+	}
+
+	var updates DeckUpdate
+	c.BindJSON(&updates)
+
+	for i := range updates.UUID {
+		var uuid = updates.UUID[i]
+
+		err = _deck.DeleteCard(uuid)
+		if err == errors.ErrNoCard {
+			c.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		}
+	}
+
+	err = _deck.UpdateDeck()
+	if err == errors.ErrDeckUpdateFailed {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusAccepted, gin.H{"message": "Successfully removed cards from deck", "deckCode": code, "count": len(updates.UUID)})
+}
