74ea9baf997bd5b4b43cf49caf74a4c8c434f503
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-17 19:04:52 -0400

    Added an additional error to cover if MongoDB returns an error on a delete operation

--- errors/errors.go
+++ errors/errors.go
@@ -18,3 +18,4 @@ var ErrCardAlreadyExist = errors.New("card: Could not complete operation. Card a
 var ErrDeckUpdateFailed = errors.New("deck: Failed to update deck with new card")
 var ErrDeckMissingId = errors.New("deck: Failed to create deck. Deck is missing a name and/or a deck code")
 var ErrDeckAlreadyExists = errors.New("deck: Failed to create deck. Deck already exists")
+var ErrDeckDeleteFailed = errors.New("deck: Failed to delete deck. Unknown server issue")
