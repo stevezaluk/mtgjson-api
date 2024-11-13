d97ebbcde86594f56c84767c66ba8696d61bceed
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-13 10:52:34 -0500

    Implemented functionaltiy for fetching the issuer url from config values in viper

--- auth/auth.go
+++ auth/auth.go
@@ -1,3 +1,16 @@
 package auth
 
+import (
+	"net/url"
 
+	"github.com/spf13/viper"
+)
+
+func GetIssuerUrl() string {
+	issuerUrl, err := url.Parse("https://" + viper.GetString("auth0.domain") + "/")
+	if err != nil {
+		panic(err) // fatal error
+	}
+
+	return issuerUrl.String()
+}
