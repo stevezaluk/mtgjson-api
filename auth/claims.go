81e78d222eb4d052742d92b4f7becc87f5bb3cc9
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-19 10:43:40 -0500

    Implemented structure for satisfying the interface off CustomClaims

--- auth/claims.go
+++ auth/claims.go
@@ -1,1 +1,26 @@
 package auth
+
+import (
+	"context"
+	"strings"
+)
+
+type CustomClaims struct {
+	Scope string `json:"scope"`
+}
+
+// need to satisfy interface requirements
+func (c CustomClaims) Validate(ctx context.Context) error {
+	return nil
+}
+
+func (c CustomClaims) HasScope(expectedScope string) bool {
+	result := strings.Split(expectedScope, " ")
+	for i := range result {
+		if result[i] == expectedScope {
+			return true
+		}
+	}
+
+	return false
+}
