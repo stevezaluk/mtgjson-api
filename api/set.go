3125443e401478e2e3a0e0bc9f85b0dbd89ad9a4
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-22 09:02:29 -0500

    Renamed context variable from 'c' to 'ctx' in set.go

--- api/set.go
+++ api/set.go
@@ -12,25 +12,25 @@ import (
 Gin handler for GET request to the set endpoint. This should not be called directly and
 should only be passed to the gin router
 */
-func SetGET(c *gin.Context) {
-	setCode := c.Query("setCode")
+func SetGET(ctx *gin.Context) {
+	setCode := ctx.Query("setCode")
 	if setCode == "" {
-		limit := limitToInt64(c.DefaultQuery("limit", "100"))
+		limit := limitToInt64(ctx.DefaultQuery("limit", "100"))
 		results, err := set.IndexSets(limit)
 		if err == errors.ErrNoSet {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "No sets available to index"})
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "No sets available to index"})
 			return
 		}
 
-		c.JSON(http.StatusOK, results)
+		ctx.JSON(http.StatusOK, results)
 		return
 	}
 
 	results, err := set.GetSet(setCode)
 	if err == errors.ErrNoSet {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Failed to find set under the requested Set Code", "setCode": setCode})
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "Failed to find set under the requested Set Code", "setCode": setCode})
 		return
 	}
 
-	c.JSON(http.StatusOK, results)
+	ctx.JSON(http.StatusOK, results)
 }
