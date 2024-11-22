ecfb442e3026086b9c23894bd94ed699bad83a81
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-22 09:04:51 -0500

    Renamed context variable from 'c' to 'ctx' in health.go

--- api/health.go
+++ api/health.go
@@ -9,6 +9,6 @@ import (
 Gin handler for GET request to the health endpoint. This should not be called directly and
 should only be passed to the gin router. Currently this just returns healthy
 */
-func HealthGET(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "healthy"})
+func HealthGET(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"status": "healthy"})
 }
