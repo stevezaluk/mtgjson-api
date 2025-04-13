ab8c5b0861d9405f522d0034740c2c612b5defdf
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-13 14:18:32 -0400

    Updated token middleware

--- middleware/token.go
+++ middleware/token.go
@@ -3,7 +3,7 @@ package middleware
 import (
 	"context"
 	"github.com/gin-gonic/gin"
-	"github.com/stevezaluk/mtgjson-sdk/user"
+	"github.com/stevezaluk/mtgjson-sdk/server"
 	"mtgjson/auth"
 	"net/http"
 	"strings"
@@ -14,7 +14,7 @@ ValidateTokenHandler Gin handler for validating tokens received from your Auth0 
 required to be passed in the request for this to properly function. If the token is valid, then it
 is stored in the gin context under 'token'. If the token is invalid, the request is aborted.
 */
-func ValidateTokenHandler() gin.HandlerFunc {
+func ValidateTokenHandler(server *server.Server) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" {
@@ -43,7 +43,7 @@ func ValidateTokenHandler() gin.HandlerFunc {
 			return
 		}
 
-		userEmail, err := user.GetEmailFromToken(tokenStr)
+		userEmail, err := server.AuthenticationManager().GetEmailFromToken(tokenStr)
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch email from access token. This is needed for establishing ownership in created objects", "err": err.Error()})
 			ctx.Abort()
