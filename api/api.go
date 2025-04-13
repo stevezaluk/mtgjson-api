7a9f8fa451e0c7996e73dc7a9c273f3b46c8ad5a
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-13 11:51:31 -0400

    Implemented support for starting and stopping the API

--- api/api.go
+++ api/api.go
@@ -3,7 +3,9 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	sloggin "github.com/samber/slog-gin"
+	"github.com/spf13/viper"
 	"github.com/stevezaluk/mtgjson-sdk/server"
+	"log/slog"
 )
 
 /*
@@ -34,4 +36,43 @@ FromConfig - Initialize the API structure using values from Viper
 */
 func FromConfig() *API {
 	return New(server.FromConfig())
+}
+
+/*
+Start - Connect to the MongoDB database and Start the API Server
+*/
+func (api *API) Start() error {
+	slog.Info("Initiating connection to MongoDB", "hostname", viper.GetString("mongo.hostname"))
+	err := api.server.Database().Connect()
+	if err != nil {
+		slog.Error("Failed to connect to MongoDB", "err", err)
+		return err
+	}
+
+	slog.Info("Starting API Server", "port", viper.GetInt("port"))
+	err = api.router.Run(":" + viper.GetString("port"))
+	if err != nil {
+		slog.Error("Failed to start API Server", "err", err)
+		return err
+	}
+
+	return nil
+}
+
+/*
+Stop - Gracefully stop the API Server and then disconnect from MongoDB
+*/
+func (api *API) Stop() error {
+	slog.Info("Shutting down API")
+	// stop receiving connections here
+	// gin router doesn't provide this natively
+
+	slog.Info("Disconnecting from MongoDB", "hostname", viper.GetString("mongo.hostname"))
+	err := api.server.Database().Disconnect()
+	if err != nil {
+		slog.Error("Failed to disconnect from MongoDB", "err", err)
+		return err
+	}
+
+	return nil
 }
