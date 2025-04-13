3183e0c368b4de23f418e65ed8d6fd054a626d42
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-13 12:14:48 -0400

    Updated descriptions for the root command

--- cmd/root.go
+++ cmd/root.go
@@ -10,9 +10,22 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Use:   "mtgjson",
-	Short: "An RESTful API built on top of the MTGJSON data set",
-	Long:  `This tool allows you to control the execution of the MTGJSON API`,
+	Use:   "mtgjson-api",
+	Short: "",
+	Long: `A command line tool for controlling the execution of the MTGJSON RESTful API.
+
+MTGJSON API is unofficial Fan Content permitted under the Fan Content Policy. 
+Not approved/endorsed by Wizards of the Coast. Portions of the materials used are property of Wizards of the Coast. 
+© Wizards of the Coast LLC.
+
+MTGJSON API not officially endorsed by MTGJSON
+
+Executing this binary with no CLI arguments will start the API with the default settings.
+Any options can be configured with either command line arguments, a config file, or environment variables.
+
+Developed and Tested on Debian-based Linux Distro's. Unconfirmed support on other operating systems`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
 }
 
 func Execute() {
